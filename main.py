2b67f08a3aa9c21ca86c2990a5fad672c38bd40f
MrNinjaPowha <joakim.gietl@gmail.com>
2022-05-19 14:27:22 +0200

    added argument's keys

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from src.main import App
 def main():
     app = App()
     app.set_file_locations(
-        'resources/wordlist/wordlist.pkl',
-        'resources/highscore/highscore.pkl',
-        'resources/settings.pkl'
+        wordlist='resources/wordlist/wordlist.pkl',
+        highscore='resources/highscore/highscore.pkl',
+        settings='resources/settings.pkl'
     )
     app.start()
 
